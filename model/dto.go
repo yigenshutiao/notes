088a90c93538800baa3dd76df41e9fadbe27ff7f
gaoxing <richardgaoxing@didichuxing.com>
2020-11-06 00:37:32 +0800

    [dev] <uutc> add some model

--- model/dto.go
+++ model/dto.go
@@ -3,8 +3,12 @@ package model
 import "time"
 
 type Note struct {
-	ID         string
-	Content    string
+	ID         string `json:"id" form:"id" validate:"required"`
+	Content    string `json:"content" form:"content" validate:"required"`
 	StartTime  time.Time
 	UpdateTime time.Time
 }
+
+type EmptyRequest struct{}
+
+type EmptyResponse struct{}
