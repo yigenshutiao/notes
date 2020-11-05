5b08b37d64a65d0ab0bb55ddd92d07f29824a7be
gaoxing <richardgaoxing@didichuxing.com>
2020-11-05 17:21:15 +0800

    [dev] <uutc> add logo

--- main.go
+++ main.go
@@ -6,9 +6,21 @@ import (
 )
 
 func main() {
+	initLOGO()
+
 	mux := initRouter()
 	err := http.ListenAndServe(":8010", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initLOGO() {
+	println(`
+             _            
+ _ __   ___ | |_ ___  ___ 
+| '_ \ / _ \| __/ _ \/ __|
+| | | | (_) | ||  __/\__ \
+|_| |_|\___/ \__\___||___/
+`)
 }
