328d8f96f1468a6004bdf9eafbc38022d28a128f
Xing Gao <18340825824@163.com>
2020-11-08 00:55:14 +0800

    [dev] <uutc> add some tag

--- model/dto.go
+++ model/dto.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type Note struct {
-	ID         string `json:"id" form:"id" validate:"required"`
-	Content    string `json:"content" form:"content" validate:"required"`
+	ID         string `json:"id" form:"id" tag:"id"`
+	Content    string `json:"content" form:"content" tag:"content"`
 	StartTime  time.Time
 	UpdateTime time.Time
 }
